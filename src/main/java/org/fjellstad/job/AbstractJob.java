713e247c1c53d7004774acb2c979ffe45e9bad45
John L Fjellstad <john@fjellstad.org>
2017-02-05 11:38:33 +0100

    adding better logging

--- src/main/java/org/fjellstad/job/AbstractJob.java
+++ src/main/java/org/fjellstad/job/AbstractJob.java
@@ -9,45 +9,51 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.Objects;
 
 public abstract class AbstractJob extends QuartzJobBean {
-    private final String jobName = getClass().getSimpleName();
-    protected final Logger logger = LoggerFactory.getLogger(getClass());
+	private final String jobName = getClass().getSimpleName();
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
-    private BatchService batchService;
+	private BatchService batchService;
 
-    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        if (!jobLock(context)) {
-            onLostElection(context);
-            return;
-        }
-        onExecuteInternal(context);
-    }
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		if (!jobLock(context)) {
+			onLostElection(context);
+			return;
+		}
+		try {
+			onExecuteInternal(context);
+		} catch (Throwable ex) {
+			logger.error("Job failed at {}", ZonedDateTime.ofInstant(context.getScheduledFireTime().toInstant(),
+			                                                         ZoneOffset.systemDefault()));
+		}
+	}
 
-    private boolean jobLock(JobExecutionContext context) {
-        try {
-            Date scheduled = context.getScheduledFireTime();
-            batchService.createJob(jobName, scheduled.toInstant().atZone(ZoneOffset.systemDefault()));
-            return true;
-        } catch(DuplicateKeyException ignore) {
-            return false;
-        }
-    }
+	private boolean jobLock(JobExecutionContext context) {
+		try {
+			Date scheduled = context.getScheduledFireTime();
+			batchService.createJob(jobName, scheduled.toInstant().atZone(ZoneOffset.systemDefault()));
+			return true;
+		} catch (DuplicateKeyException ignore) {
+			return false;
+		}
+	}
 
-    protected abstract void onExecuteInternal(JobExecutionContext context) throws JobExecutionException;
+	protected abstract void onExecuteInternal(JobExecutionContext context) throws JobExecutionException;
 
-    @SuppressWarnings("WeakerAccess")
-    protected void onLostElection(JobExecutionContext context) {
-    }
+	@SuppressWarnings("WeakerAccess")
+	protected void onLostElection(JobExecutionContext context) {
+	}
 
 	/**
 	 * Used by Quartz scheduler to set service
 	 */
 	@SuppressWarnings("unused")
-    public void setBatchService(BatchService batchService) {
-        Objects.requireNonNull(batchService, "batchService cannot be null");
-        this.batchService = batchService;
-    }
+	public void setBatchService(BatchService batchService) {
+		Objects.requireNonNull(batchService, "batchService cannot be null");
+		this.batchService = batchService;
+	}
 }
--- src/main/java/org/fjellstad/job/NodeJob.java
+++ src/main/java/org/fjellstad/job/NodeJob.java
@@ -3,12 +3,15 @@ package org.fjellstad.job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
-import java.time.LocalDateTime;
-
 public class NodeJob extends AbstractJob {
 
 	@Override
 	protected void onExecuteInternal(JobExecutionContext context) throws JobExecutionException {
-		logger.info("onExecuteInternal at {}", LocalDateTime.now());
+		logger.info("Won election at {}", context.getScheduledFireTime());
+	}
+
+	@Override
+	protected void onLostElection(JobExecutionContext context) {
+		logger.info("Lost election at {}", context.getScheduledFireTime());
 	}
 }
